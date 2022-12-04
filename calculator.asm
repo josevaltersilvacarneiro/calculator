05d5ef83bea5ce631b6e686287b70c24ec7a3817
Jose Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-04 07:27:26 -0800

    Starting

---
+++ calculator.asm
@@ -0,0 +1,12 @@
+global _start
+
+section.data
+message: db 'hello, world', 10
+
+section.text
+_start:
+	mov	rax, 1		; The system call number must be stored in rax
+	mov	rdi, 1		; argument #1 in rdi: where write (descritor) ?
+	mov	rsi, message	; argument #2 in rsi: where start the string  ?
+	mov	rdx, 14		; argument #3 in rdx: How much bytes must be  ?
+	syscall			: this instruction mades a system cal
