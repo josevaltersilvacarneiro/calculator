d8137bdf6b8117fc732824f0f55ae3a71747bc78
Jose Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-11 14:27:20 -0800

    The bug 'segmentation fault' was fixed

--- calculator.asm
+++ calculator.asm
@@ -1,12 +1,16 @@
-global _start
-
 section .data
 message: db 'hello, world', 10
 
 section .text
+global _start
+
 _start:
 	mov	rax, 1		; The system call number must be stored in rax
 	mov	rdi, 1		; argument #1 in rdi: where write (descritor) ?
 	mov	rsi, message	; argument #2 in rsi: where start the string  ?
 	mov	rdx, 14		; argument #3 in rdx: How much bytes must be  ?
 	syscall			; this instruction mades a system cal
+
+	mov	rax, 60		; number of the syscall 'exit'
+	xor	rdi, rdi
+	syscall
